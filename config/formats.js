28fd461d573d3d7dc8f541da5e8da818dcd5c9bb
amish995 <amish995@hotmail.com>
2019-07-03 21:19:38 +0800

    Lele

--- config/formats.js
+++ config/formats.js
@@ -18,7 +18,11 @@ let Formats = [
 		unbanlist: ['Genesect', 'Blaziken', 'Giratina-Origin', 'Landorus', 'Marshadow', 'Deoxys-Defense', 'Deoxys-Speed', 'Darkrai', 'Aegislash'],
 		validateSet(set, teamHas) {
 			let problems = this.validateSet(set, teamHas);
-			
+			if (problems){
+				problems = problems.filter(e => e !== 'Tapu Lele\'s Hidden Ability is unreleased.')
+				problems = problems.filter(e => e !== 'Tapu Lele is only obtainable from events - it needs to match one of its events, such as:')
+				problems = problems.filter(e => e !== 'Tapu Lele must not have its Hidden Ability to be from its event #1.')
+			}
 			return problems;
 		},
 	},
